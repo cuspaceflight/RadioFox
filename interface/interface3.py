3f22294c35fcd14595d891cbf4611360c8f61ec5
Oli Lane <olil4ne@gmail.com>
2012-11-24 15:25:06 +0000

    Formatted to conform with PEP8, commented unused imports.

--- interface/interface3.py
+++ interface/interface3.py
@@ -1,12 +1,12 @@
-from Tkinter import *;
+from Tkinter import *
 
-import serial
-import os
-import time
+#import serial
+#import os
+#import time
 import math
 import json
 
-import sched
+#import sched
 
 from urllib import urlopen
 #from threading import Timer
@@ -17,12 +17,17 @@ from urllib import urlopen
 # ent.grid()
 #
 # and retrieve the values using:
-#	var.get()
+#    var.get()
 #
 root = Tk()
 callsign = StringVar()
 distance = StringVar()
 balloonPositions = []
+
+
+# Define constants for easy tweaking
+balloonPositionPollPeriod = 20000  # ms between balloon position updates
+
 
 # Notes on these event handlers:
 # Tkinter does not support custom events. So we'll have to create
@@ -30,140 +35,151 @@ balloonPositions = []
 # and invoke these callbacks manually.
 
 events = {
-	'balloonPositionUpdated': [],
-	'rotatorOrientationUpdated': [],
-	'distanceUpdated': [], # shortcut for both balloonPositionUpdated and rotatorPositionUpdated
-	'rotatorPositionUpdated': [],
-	'rotatorTimeout': [],
-	'balloonTimeout': []
+    'balloonPositionUpdated': [],
+    'rotatorOrientationUpdated': [],
+    'distanceUpdated': [],  # shortcut for both balloonPositionUpdated and rotatorPositionUpdated
+    'rotatorPositionUpdated': [],
+    'rotatorTimeout': [],
+    'balloonTimeout': []
 }
 
+
 def invoke_event(event_name, args):
-	global app, events
-	
-	for evt in events[event_name]:
-		#app.after(0, evt(args))
-		evt(args)
+    global app, events
 
-def UpdateBalloonPositionList ():
-	"""Invoked when balloon position has been updated.
-	
-	This updates the list of balloon positions"""
-	
-	
-	pass
-	
-def UpdateBalloonDistance ():
-	"""Invoked when rotator position has been updated (should it ever be?).
-	
-	This updates the balloon's position on GUI"""
-	global distance
+    for evt in events[event_name]:
+        #app.after(0, evt(args))
+        evt(args)
 
-	# calculate distance?
-	our_lat = float(self.controls.ant_lat.GetValue())
-	our_lng = float(self.controls.ant_lon.GetValue())
-	our_alt = float(self.controls.ant_alt.GetValue())
-	lat = float(lat)
-	lng = float(lng)
-	alt = float(alt)
-	d = distance.distance((our_lat, our_lng), (lat, lng)).km
-	d_alt = (alt - our_alt)/1000
-	dist = math.sqrt(d**2 + d_alt**2)
-	print "Distance to payload: {0}".format(dist)
-	
-	distance.set(format(dist))
 
-def UpdateRotatorPositionList (lat, long, alt):
-	"""Invoked when rotator position has been updated (should it ever be?).
-	
-	This updates the list of balloon positions"""
-	pass
+def UpdateBalloonPositionList():
+    """Invoked when balloon position has been updated.
 
-def UpdateRotatorOrientation ():
-	"""Invoked when rotator orientation (az, el) has been updated (should it ever be?).
-	
-	This updates the list of balloon positions"""
-	pass
+    This updates the list of balloon positions"""
 
-def UpdateGraphics (lat, long, alt):
-	"""Invoked when either rotator or balloon position has been updated.
-	
-	This updates the list of balloon positions"""
-	pass
+    pass
+
+
+def UpdateBalloonDistance():
+    """Invoked when rotator position has been updated (should it ever be?).
+
+    This updates the balloon's position on GUI"""
+    global distance
+
+    # calculate distance?
+    our_lat = float(self.controls.ant_lat.GetValue())
+    our_lng = float(self.controls.ant_lon.GetValue())
+    our_alt = float(self.controls.ant_alt.GetValue())
+    lat = float(lat)
+    lng = float(lng)
+    alt = float(alt)
+    d = distance.distance((our_lat, our_lng), (lat, lng)).km
+    d_alt = (alt - our_alt) / 1000
+    dist = math.sqrt(d ** 2 + d_alt ** 2)
+    print "Distance to payload: {0}".format(dist)
+
+    distance.set(format(dist))
+
+
+def UpdateRotatorPositionList(lat, long, alt):
+    """Invoked when rotator position has been updated (should it ever be?).
+
+    This updates the list of balloon positions"""
+    pass
+
+
+def UpdateRotatorOrientation():
+    """Invoked when rotator orientation (az, el) has been updated (should it ever be?).
+
+    This updates the list of balloon positions"""
+    pass
+
+
+def UpdateGraphics(lat, long, alt):
+    """Invoked when either rotator or balloon position has been updated.
+
+    This updates the list of balloon positions"""
+    pass
+
 
 def SendAlert():
-	pass
+    pass
+
 
 def FetchBalloonPosition():
-	global app
-	global callsign
-	global balloonPositions
-	
-	print "Fetching latest balloon pos"
-	alt = "0"
-	callsign_wanted = callsign.get()
-	url = "http://habitat.habhub.org/habitat/_design/habitat/_view/" + \
-		"payload_telemetry?startkey=[%22{0}%22,%22latest%22]&descending=true&limit=1&include_docs=true".format(callsign_wanted)
-	data = json.loads(urlopen(url).read())["rows"][0]["doc"]["data"]
-	callsign, lat, lng, alt = data["payload"], data["latitude"], data["longitude"], data["altitude"]
-	lat = str(lat)
-	lng = str(lng)
-	alt = str(alt)
-	
-	# Question: Is there a time field?
-	
-	print "Got callsign=%s, lat=%s, lng=%s, alt=%s" % (callsign, lat, lng, alt)
-	#print "This ones a keeper"
-	if callsign == callsign_wanted:
-		print "Callsign match, setting balloon pos"
-		balloonPositions.insert(0, (lat, lng, alt))
-		
-		if len(balloonPositions) > 10:
-			# cull the last positions to 10
-			# TODO: need to check if the last 10 readings were taken at the same time
-			balloonPositions = balloonPositions[0:9]
-		
-		invokeEvent('balloonPositionUpdated', [])
-	else:
-		if False: ## e.g. TIME SINCE LAST UPDATE > 10 mins
-			invokeEvent('balloonTimeout', [])
-			pass
-		print "Callsign not matched, ignoring"
-		#self.SetBalloonPos(None)
-	
-	# poll again after some time. Time is in milliseconds
-	app.after( 20000, FetchBalloonPosition )
+    global app
+    global callsign
+    global balloonPositions
+    global balloonPositionPollPeriod
+
+    print "Fetching latest balloon pos"
+
+    alt = "0"
+    callsign_wanted = callsign.get()
+    url = "http://habitat.habhub.org/habitat/_design/habitat/_view/" + \
+          "payload_telemetry?startkey=[%22{0}%22,%22latest%22]&descending=true&limit=1&include_docs=true".format(callsign_wanted)
+    data = json.loads(urlopen(url).read())["rows"][0]["doc"]["data"]
+    callsign, lat, lng, alt = data["payload"], data["latitude"], data["longitude"], data["altitude"]
+    lat = str(lat)
+    lng = str(lng)
+    alt = str(alt)
+
+    # Question: Is there a time field?
+
+    print "Got callsign=%s, lat=%s, lng=%s, alt=%s" % (callsign, lat, lng, alt)
+
+    if callsign == callsign_wanted:
+        print "Callsign match, setting balloon pos"
+        balloonPositions.insert(0, (lat, lng, alt))
+
+        if len(balloonPositions) > 10:
+            # cull the last positions to 10
+            # TODO: need to check if the last 10 readings were taken at the same time
+            balloonPositions = balloonPositions[0:9]
+
+        invokeEvent('balloonPositionUpdated', [])
+    else:
+        if False:  # e.g. TIME SINCE LAST UPDATE > 10 mins
+            invokeEvent('balloonTimeout', [])
+            pass
+        print "Callsign not matched, ignoring"
+        #self.SetBalloonPos(None)
+
+    # poll again after (balloonPositionPollPeriod) milliseconds
+    app.after(balloonPositionPollPeriod, FetchBalloonPosition)
+
 
 def FetchRotatorPosition():
-	"""Fetches the rotator position, if new values are available.
-	Or ping otherwise."""
-	pass
-	
+    """Fetches the rotator position, if new values are available.
+    Or ping otherwise."""
+    pass
+
+
 def CreateGraphics():
-	global app, root, callsign
-	
-	app = Frame(root)
-	app.grid()
-	
-	ent_callsign = Entry(app, textvariable=callsign)
-	ent_callsign.grid()
-	
-	ent_distance = Entry(app, textvariable=distance)
-	ent_distance.grid()
-	
-	# Before invoking mainloop, we need to ensure that our app will
-	# call the timer-based procedures
-	app.after(1000, FetchBalloonPosition)
-	app.after(1000, FetchRotatorPosition)
-	
-	app.mainloop()
+    global app, root, callsign
+
+    app = Frame(root)
+    app.grid()
+
+    ent_callsign = Entry(app, textvariable=callsign)
+    ent_callsign.grid()
+
+    ent_distance = Entry(app, textvariable=distance)
+    ent_distance.grid()
+
+    # Before invoking mainloop, we need to ensure that our app will
+    # call the timer-based procedures
+    app.after(1000, FetchBalloonPosition)
+    app.after(1000, FetchRotatorPosition)
+
+    app.mainloop()
 
 # Attach events
-events['balloonPositionUpdated'].extend( [UpdateBalloonPositionList] )
-events['rotatorOrientationUpdated'].extend( [UpdateRotatorOrientation] )
-events['rotatorPositionUpdated'].extend( [UpdateRotatorPositionList] )
-events['distanceUpdated'].extend( [UpdateBalloonDistance] )
-events['rotatorTimeout'].extend( [SendAlert] )
-events['balloonTimeout'].extend( [SendAlert] )
+events['balloonPositionUpdated'].extend([UpdateBalloonPositionList])
+events['rotatorOrientationUpdated'].extend([UpdateRotatorOrientation])
+events['rotatorPositionUpdated'].extend([UpdateRotatorPositionList])
+events['distanceUpdated'].extend([UpdateBalloonDistance])
+events['rotatorTimeout'].extend([SendAlert])
+events['balloonTimeout'].extend([SendAlert])
 
-CreateGraphics() # Main loop starts
+CreateGraphics()  # Main loop starts
